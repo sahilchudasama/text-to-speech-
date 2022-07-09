e533f913fad09bcedd8d26a6c0b2d5a52413e539
sahilchudasama <98327653+sahilchudasama@users.noreply.github.com>
2022-07-09 14:58:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector('.speak_btn');
+btn.onclick = () => {
+    let txt = document.querySelector('.enter_text'
+    ).value;
+let speech = new SpeechSynthesisUtterance();
+speech.lang = 'en_US';
+speech.text = txt;
+speech. rate = 1;
+speech.volume = 1;
+speech.pitch = 1;
+speechSynthesis.speak (speech);
+}
